1a8f4aa33ac72013eaa92f465da0fd5d2306b6a7
Dongwon Hwang <61178312+Dongwon-tuna@users.noreply.github.com>
2024-02-10 02:27:50 +0900

    XOR encryption

---
+++ encryption.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//xor 연산을 통한 암호화
+
+int main() {
+    int in, res;
+    scanf("%d", &in);
+    
+    //% 나머지 /는 몫
+
+    while (in != 0)
+    {
+        res = in%2;
+        in = in/2;
+        printf("%d\n",in);
+    }
+    printf("나누기 완료");
+    return 0;
+
+}
