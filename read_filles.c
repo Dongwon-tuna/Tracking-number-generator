c23717fbf04986f08cdd885c300d48f41306a2d5
Dongwon Hwang <61178312+Dongwon-tuna@users.noreply.github.com>
2024-02-09 23:56:17 +0900

    Parsing_Zipcod_data

--- read_filles.c
+++ read_filles.c
@@ -1,34 +1,51 @@
 #pragma warning(disable :4996)
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define MAX_FIELDS 30  
-/*
-typedef struct info_list
+
+//------------------------------------------------------------------------
+struct Zipcode_data
 {
-    char zipcode[5];
-    char doo[10];
-    char doo_eng[30];
-    char city[10];
-    char city_eng[20];
-    char eup[10];
-    char eup_eng[20];
-    char road_code[13];
-    char road_add[15];
-    char road_add_eng[20];
+    char zipcode[10];
+    char d[30];
+    char d_eng[30];
+    char c[20];
+    char c_eng[20];
 
-};list;
-*/
+};
 
+//-------------------------------------------------------------------------
+int line_length(){
+   FILE *fp;
+   fp = fopen("read.txt", "r");
+   char tmp;
+   int cnt = 1;
 
+   if (fp == NULL) {
+      printf("Failed");
+   }
 
+   while (fscanf(fp, "%c", &tmp) != EOF) {
+      if (tmp == '\n')
+         cnt++;
+   }
+   
+   return cnt; 
+}
+
+//----------------------------------------------------------------------------
 int main()
 {
+    struct Zipcode_data *data;
+    int row =0;
     FILE* fp;
     fp = fopen("read.txt", "r");
     char frame[256];
     char buffer[256];
-    char *info_arr[8][30];
-
+    int row_num = line_length();
+    data = (struct Zipcode_data *)malloc(sizeof(struct Zipcode_data)*row_num);
+    
 
     while (fgets(buffer, 256, fp))
     {
@@ -39,23 +56,29 @@ int main()
 
         while (token != NULL && field_count < MAX_FIELDS) {
        
-        fields[field_count] = token;
+            fields[field_count] = token;
 
-        token = strtok(NULL, "|");
+            token = strtok(NULL, "|");
 
-        field_count++;
+            field_count++;
         }
 
-        
-        for (int i = 0; i < field_count; i++) {
-            if (fields[i] != NULL) {
-                printf("Field %d: %s\n", i , fields[i]);
-            } else {
-                printf("Field %d: NULL\n", i );
-            }
-        }
-        
+        strcpy((data + row)->zipcode, fields[0]);
+        strcpy((data + row)->d, fields[1]);
+        strcpy((data + row)->d_eng, fields[2]);
+        strcpy((data + row)->c, fields[3]);
+        strcpy((data + row)->c_eng, fields[4]);
+
+        row++;
+
     }
+
+    for (int i = 0; i < row_num -1; i++)
+    {
+        printf("1: %s 2: %s 3: %s 4: %s 5: %s\n",(data+i)->zipcode,(data+i)->d, (data+i)->d_eng, (data+i)->c,(data+i)->c_eng );
+    }
+    
     fclose(fp);
+    free(data);
 }
 
