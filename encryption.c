9a07e44ea983769876b84f2f5af026b7a22854a9
Dongwon Hwang <61178312+Dongwon-tuna@users.noreply.github.com>
2024-02-13 21:07:03 +0900

    Update encryption.c

--- encryption.c
+++ encryption.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-//Xor 연산을 통한 암호화
+int div_number(int num){
+    int res;
+    int cnt = 0;
+    while (num != 0)
+    {
+        res = num%2;
+        num = num/2;
+        cnt ++;
+    }
+    return cnt;
+}
+
+int *dec_to_bin(int num){
+    static int array[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+    array[3] = num;
+    return array;
+
+}
 
 int main() {
-    int in, res;
-    scanf("%d", &in);
+    int in;
     
-    //% 나머지 /는 몫
-
-    while (in != 0)
-    {
-        res = in%2;
-        in = in/2;
-        printf("%d\n",in);
-    }
+ 
+    scanf("%d", &in);
     printf("나누기 완료");
+    printf(": %d\n",div_number(in));
+    int *arr = dec_to_bin(div_number(in));
+    for (int i = 0; i < 12; i++)
+    {
+        
+        printf("%d",arr[i]);
+    }
+    
+
     return 0;
 
 }
