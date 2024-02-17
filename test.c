861d23de9f1c269fc59405d8ff3e18f4d16d6d2d
Dongwon Hwang <61178312+Dongwon-tuna@users.noreply.github.com>
2024-02-17 22:49:53 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void){
-    char s[] = "hi";
-    printf("%s",s);
+char **parsing() {
+    static char *arr1[3] = {"hi", "hiii", "hiiiq"};
+    return arr1;
+}
+
+int main(void) {
+    // char str[] = "Block D mask";
+    // char *ptr = strtok(str," ");
+    // char *return1[3];
+    // int cnt = 0;
+    // while (ptr != NULL)
+    // {
+    //     printf("%s\n", ptr);
+        
+    //     return1[cnt] = ptr;
+    //     printf("%s\n",return1[cnt]);
+    //     ptr = strtok(NULL," ");
+    //     cnt ++;
+
+    // }
+
+    char **getpar = parsing();
+
+    for (int i = 0; i < 3; i++) {
+        printf("%s\n", getpar[i]);
+    }
+
+    return 0;
 }
