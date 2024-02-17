5fdaf74dc24ee634df09655e5c931b2f551f17d0
Dongwon Hwang <61178312+Dongwon-tuna@users.noreply.github.com>
2024-02-17 22:49:39 +0900

    Create binary_operations.h

---
+++ binary_operations.h
@@ -0,0 +1,11 @@
+// binary_operations.h
+#ifndef BINARY_OPERATIONS_H
+#define BINARY_OPERATIONS_H
+
+int div_number(int num);
+int *dec_to_bin(int num);
+int *key_value(int num);
+int *xor_process(int arr1[], int arr2[]);
+int bin_to_dec(int arr[]);
+
+#endif // BINARY_OPERATIONS_H
