9abe77927da3ea2bfb2c17b34f568904d9bc40f1
Dongwon Hwang <61178312+Dongwon-tuna@users.noreply.github.com>
2024-02-19 00:10:38 +0900

    to json data

--- test.c
+++ test.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdbool.h>
+#include "parson.h"
 
-char **parsing() {
-    static char *arr1[3] = {"hi", "hiii", "hiiiq"};
-    return arr1;
+void createAndAppendJSON(JSON_Array *mainArray, const char* title, int year, int runtime, const char* genre, const char* director, const char** actors, int actorsCount, double imdbRating, bool koreaRelease) {
+    JSON_Value *rootValue = json_value_init_object();
+    JSON_Object *rootObject = json_value_get_object(rootValue);
+
+    json_object_set_string(rootObject, "Title", title);
+    json_object_set_number(rootObject, "Year", year);
+    json_object_set_number(rootObject, "Runtime", runtime);
+    json_object_set_string(rootObject, "Genre", genre);
+    json_object_set_string(rootObject, "Director", director);
+
+    json_object_set_value(rootObject, "Actors", json_value_init_array());
+    JSON_Array *actorsArray = json_object_get_array(rootObject, "Actors");
+    for (int i = 0; i < actorsCount; ++i) {
+        json_array_append_string(actorsArray, actors[i]);
+    }
+
+    json_object_set_number(rootObject, "imdbRating", imdbRating);
+    json_object_set_boolean(rootObject, "KoreaRelease", koreaRelease);
+
+    json_array_append_value(mainArray, rootValue);
 }
 
-int main(void) {
-    // char str[] = "Block D mask";
-    // char *ptr = strtok(str," ");
-    // char *return1[3];
-    // int cnt = 0;
-    // while (ptr != NULL)
-    // {
-    //     printf("%s\n", ptr);
-        
-    //     return1[cnt] = ptr;
-    //     printf("%s\n",return1[cnt]);
-    //     ptr = strtok(NULL," ");
-    //     cnt ++;
+int main() {
+    JSON_Value *mainValue = json_value_init_array();
+    JSON_Array *mainArray = json_value_get_array(mainValue);
 
-    // }
+    const char* actors1[] = {"Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page", "Tom Hardy", "Ken Watanabe"};
+    const char* actors2[] = {"Actor1", "Actor2", "Actor3"};
 
-    char **getpar = parsing();
+    createAndAppendJSON(mainArray, "Inception", 2010, 148, "Sci-Fi", "Christopher Nolan", actors1, 5, 8.8, true);
+    createAndAppendJSON(mainArray, "Another Movie", 2015, 120, "Drama", "Director X", actors2, 3, 7.5, false);
 
-    for (int i = 0; i < 3; i++) {
-        printf("%s\n", getpar[i]);
-    }
+    json_serialize_to_file_pretty(mainValue, "example_combined.json");
+
+    json_value_free(mainValue);
 
     return 0;
 }
